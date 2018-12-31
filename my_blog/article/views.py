c72390967ebae80fdcfbf71a333737fa1492c857
Jing Zhao <Jing_I@update.mah>
2018-12-31 17:30:30 -0500

    markdown

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
-from django.shortcuts import render
+# 引入markdown模块
+import markdown
 # 导入数据模型ArticlePost
 from .models import ArticlePost
 def article_list(request):
@@ -15,6 +16,16 @@ def article_detail(request, id):
     # 取出相应的文章
     article = ArticlePost.objects.get(id=id)
     # 需要传递给模板的对象
-    context = { 'article': article }
+    #context = { 'article': article }
     # 载入模板，并返回context对象
+    #return render(request, 'article/detail.html', context)
+    # 将markdown语法渲染成html样式
+    article.body = markdown.markdown(article.body,
+        extensions=[
+        # 包含 缩写、表格等常用扩展
+        'markdown.extensions.extra',
+        # 语法高亮扩展
+        'markdown.extensions.codehilite',
+        ])
+    context = { 'article': article }
     return render(request, 'article/detail.html', context)
