c6e886771b8e7607968bd88c1a79128a1cc1b928
Jing Zhao <Jing_I@update.mah>
2019-01-03 11:57:17 -0500

    使用Form表单类发表新文章

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
-
-# 引入markdown模块
-import markdown
-# 导入数据模型ArticlePost
 from .models import ArticlePost
+
+from django.shortcuts import render, redirect
+# 引入HttpResponse
+from django.http import HttpResponse
+# 引入刚才定义的ArticlePostForm表单类
+from .forms import ArticlePostForm
+# 引入User模型
+from django.contrib.auth.models import User
+
 def article_list(request):
     # 取出所有博客文章
     articles = ArticlePost.objects.all()
@@ -20,12 +24,40 @@ def article_detail(request, id):
     # 载入模板，并返回context对象
     #return render(request, 'article/detail.html', context)
     # 将markdown语法渲染成html样式
-    article.body = markdown.markdown(article.body,
-        extensions=[
+    #article.body = markdown.markdown(article.body,
+        #extensions=[
         # 包含 缩写、表格等常用扩展
-        'markdown.extensions.extra',
+        #'markdown.extensions.extra',
         # 语法高亮扩展
-        'markdown.extensions.codehilite',
-        ])
+        #'markdown.extensions.codehilite',
+        #])
     context = { 'article': article }
     return render(request, 'article/detail.html', context)
+
+# 写文章的视图
+def article_create(request):
+    # 判断用户是否提交数据
+    if request.method == "POST":
+        # 将提交的数据赋值到表单实例中
+        article_post_form = ArticlePostForm(data=request.POST)
+        # 判断提交的数据是否满足模型的要求
+        if article_post_form.is_valid():
+            # 保存数据，但暂时不提交到数据库中
+            new_article = article_post_form.save(commit=False)
+            # 指定数据库中 id=1 的用户为作者
+            new_article.author = User.objects.get(id=1)
+            # 将新文章保存到数据库中
+            new_article.save()
+            # 完成后返回到文章列表
+            return redirect("article:article_list")
+        # 如果数据不合法，返回错误信息
+        else:
+            return HttpResponse("表单内容有误，请重新填写。")
+    # 如果用户请求获取数据
+    else:
+        # 创建表单类实例
+        article_post_form = ArticlePostForm()
+        # 赋值上下文
+        context = { 'article_post_form': article_post_form }
+        # 返回模板
+        return render(request, 'article/create.html', context)
